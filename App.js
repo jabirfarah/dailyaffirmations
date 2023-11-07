96948940c7593f49d013feb4e53306839700d195
jabirfarah <86932547+jabirfarah@users.noreply.github.com>
2023-11-07 22:24:26 +0000

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Button } from 'react-native';
+import AffirmationFetch from './affirmationFetch';
 
 export default function App() {
+  const image = {uri: "https://external-content.duckduckgo.com/iu/?u=https://wallpaperaccess.com/full/1155299.png&f=1&nofb=1&ipt=0be5032c3fda98333cb1b4a215856ea09189f5f2299878ee30507c33165b3839&ipo=images"}
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={image} style={styles.image}>
+        <Text style={styles.text}>
+          <AffirmationFetch/>
+          
+        </Text>
+        <Text style={styles.buttons}>
+          <Button title="❤️" />
+          <Button title="🔖" />
+          <Button title="📥" />
+        </Text>
+      </ImageBackground>
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -13,8 +26,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 29,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 30,
+  },
+  buttons: {
+    flexDirection: 'column',
+    justifyContent: "flex-end",
+    borderWidth: 2,
+    alignContent: "center",
+    alignItems: "center",
+    margin: "auto",
+    padding: "auto"
+    
+    
   },
 });
---
+++ affirmationFetch.js
@@ -0,0 +1,33 @@
+// import react native
+import React, { useEffect } from 'react'
+import { Text, } from 'react-native'
+import { useState } from 'react'
+
+
+const AffirmationFetch = () => {
+
+    const [affirmation, setAffirmation] = useState('')
+
+    const today = new Date();
+    const midnight = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+    console.log(midnight)
+
+    useEffect(() => {
+        const something = async () => {
+
+            const res = await fetch('https://www.affirmations.dev/');
+            const json = await res.json();
+            const jsonParse = await JSON.parse(JSON.stringify(json))
+            console.log(jsonParse)
+            setAffirmation(jsonParse.affirmation)
+        }
+       something();
+    }, [])
+    
+    
+  return (
+    <Text>{affirmation}</Text>
+  )
+}
+
+export default AffirmationFetch
