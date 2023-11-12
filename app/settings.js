fa77b2d7ce7a754b228d69c580358158a7e2080e
jabirfarah <86932547+jabirfarah@users.noreply.github.com>
2023-11-12 22:19:06 +0000

    expo router links

---
+++ app/settings.js
@@ -0,0 +1,10 @@
+import { ImageBackground, StyleSheet, Text, View, Button } from 'react-native';
+
+
+export default function Settings() {
+    return (
+      <View>
+        <Text>THIS IS A SETTINGS PAGE!</Text>
+      </View>
+    );
+  }
