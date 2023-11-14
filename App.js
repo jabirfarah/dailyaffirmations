843e74a0e4767f0f8d2a8478157a0fe2feb5cf6d
jabirfarah <86932547+jabirfarah@users.noreply.github.com>
2023-11-14 20:51:43 +0000

    scrollview add

--- App.js
+++ App.js
@@ -1,30 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, View, Button } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Button, ScrollView, SafeAreaView } from 'react-native';
 import AffirmationFetch from './affirmationFetch';
 import { Link } from 'expo-router';
 
 
  function App() {
   const image = {uri: "https://external-content.duckduckgo.com/iu/?u=https://wallpaperaccess.com/full/1155299.png&f=1&nofb=1&ipt=0be5032c3fda98333cb1b4a215856ea09189f5f2299878ee30507c33165b3839&ipo=images"}
+  
   return (
-    <View style={styles.container}>
-      <ImageBackground source={image} style={styles.image}>
-        <Text style={styles.text}>
-          <AffirmationFetch/>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <ImageBackground source={image} style={styles.image}>
+          <Text style={styles.text}>
+            <AffirmationFetch/>
+          </Text>
+          <Text style={styles.buttons}>
+            <Button title="❤️"/>
+            <Button title="🔖"/>
+            <Button title="📥"/>
+          </Text>
+          <Text>
+          <Link href={""} isReady="true">GO TO THE SETTINGS PAGE</Link>
         </Text>
-        <Text style={styles.buttons}>
-          <Button title="❤️" />
-          <Button title="🔖" />
-          <Button title="📥" />
-        </Text>
-        <Text>
-        <Link href={l} isReady="true">GO TO THE SETTINGS PAGE</Link>
-      </Text>
-      <StatusBar style="auto" />
-      </ImageBackground>
-      
-      
-    </View>
+        <StatusBar style="auto" />
+        </ImageBackground>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -32,6 +33,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
+    paddingTop: StatusBar.currentHeight,
+
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
   },
   image: {
     flex: 1,
