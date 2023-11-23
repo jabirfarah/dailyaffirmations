91a709d23e588f5e1ac5062cfc74e21fee29dded
jabirfarah <86932547+jabirfarah@users.noreply.github.com>
2023-11-23 00:21:25 +0000

    removed some comments

--- MyPager.js
+++ MyPager.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, View, Text, Button, Share } from 'react-native';
 import PagerView from 'react-native-pager-view';
-import AffirmationFetch from './affirmationFetch';
 import * as Sharing from 'expo-sharing';
 
+const AffirmationFetch = () => {
 
+  const [affirmation, setAffirmation] = useState([])
+
+  const today = new Date();
+  const midnight = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+  console.log(midnight)
+
+
+  useEffect(() => {
+      const something = async () => {
+          const res = await fetch('https://www.affirmations.dev/');
+          const json = await res.json();
+          const jsonParse = await JSON.parse(JSON.stringify(json));
+          setAffirmation(jsonParse.affirmation);
+          for (i = 0; i < 3; i++) {
+            setAffirmation(...affirmation, jsonParse.affirmation);
+            console.log(affirmation);
+            // make it so that a different affirmation pops up now
+            
+            console.log(jsonParse);
+          }
+      }
+     something();
+  }, [])
+  console.log(affirmation);
+
+
+return (
+  <Text>{affirmation}</Text>
+)
+}
 
 const MyPager = () => {
 
@@ -22,7 +52,7 @@ const MyPager = () => {
                   // shared
               }
           } else if (result.action === Share.dismissedAction) {
-              // dismissed
+             console.log("dismissed");
           }
       
       } catch(error) {
@@ -30,18 +60,22 @@ const MyPager = () => {
       }
   }
 
+  // You can make it so that after a certain amount of 
+
   return (
     <View style={{ flex: 1 }}>
       <PagerView style={styles.viewPager} initialPage={0} orientation={"vertical"}>
         <View style={styles.page} key="1">
-          <Text style={styles.text}><AffirmationFetch/>.</Text>
+          <Text style={styles.text}>{AffirmationFetch.affirmation}.</Text>
           <Button title='SHARE BUTTON' onPress={share}></Button>
         </View>
         <View style={styles.page} key="2">
           <Text style={styles.text}><AffirmationFetch/>.</Text>
+          <Button title='SHARE BUTTON' onPress={share}></Button>
         </View>
         <View style={styles.page} key="3">
           <Text style={styles.text}><AffirmationFetch/>.</Text>
+          <Button title='SHARE BUTTON' onPress={share}></Button>
         </View>
       </PagerView>
     </View>
