533a4ff8c1c8709724bb99dbabbaefc9c83685b5
jabirfarah <86932547+jabirfarah@users.noreply.github.com>
2023-12-04 18:55:13 +0000

    Add BottomSheet functionality

--- components/BottomSheet.jsx
+++ components/BottomSheet.jsx
@@ -1,24 +1,42 @@
 import { Dimensions } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
-import Animated, { useSharedValue } from 'react-native-reanimated';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
 
 const { height: SCREEN_HEIGHT } = Dimensions.get('window');
+const MAX_TRANSLATE_Y = -SCREEN_HEIGHT + 50;
 
 const BottomSheet = () => {
 
     const translateY = useSharedValue(0)
 
-    const gesture = Gesture.Pan().onUpdate((event) => {
-        console.log(event.translationY)
-        translateY.value = event.translationY
-    })
+    const context = useSharedValue({ y: 0 }); 
+
+    const gesture = Gesture.Pan().onStart(() => {
+        context.value = { y: translateY.value }; 
+    }).onUpdate((event) => {
+        translateY.value = event.translationY + context.value.y;
+        translateY.value = Math.max(translateY.value, MAX_TRANSLATE_Y );
+
+        console.log(translateY.value)
+    });
+
+    const rBottomSheetStyle = useAnimatedStyle(() => {
+         return {
+            transform: [{ translateY: translateY.value }],
+            
+         } 
+    });
+ 
+    useEffect(() => {
+        translateY.value = withSpring(-SCREEN_HEIGHT /3, {damping: 50, duration: 1000 })
+    }, [])
 
   return ( 
     <>
          <GestureDetector gesture={gesture}>
-            <Animated.View style={styles.bottomSheetContainer}>
+            <Animated.View style={[styles.bottomSheetContainer, rBottomSheetStyle ]}>
                 <View style={styles.line}/>
 
             </Animated.View>
@@ -37,7 +55,7 @@ const styles = StyleSheet.create({
         width: '100%',
         backgroundColor: 'white',
         position: 'absolute',
-        top: SCREEN_HEIGHT / 1.5,
+        top: SCREEN_HEIGHT,
         borderRadius: 25
     },
     line: {
