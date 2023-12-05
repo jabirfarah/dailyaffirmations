6108a1cc43b31b8eb4d80aaefb4fe7ecc7bbd2a5
jabirfarah <86932547+jabirfarah@users.noreply.github.com>
2023-12-05 21:53:06 +0000

    BottomSheet breakpoints

--- components/BottomSheet.jsx
+++ components/BottomSheet.jsx
@@ -2,7 +2,7 @@ import { Dimensions } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useEffect } from 'react'
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
-import Animated, { useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
+import Animated, { Extrapolate, interpolate, useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
 
 const { height: SCREEN_HEIGHT } = Dimensions.get('window');
 const MAX_TRANSLATE_Y = -SCREEN_HEIGHT + 50;
@@ -13,24 +13,42 @@ const BottomSheet = () => {
 
     const context = useSharedValue({ y: 0 }); 
 
-    const gesture = Gesture.Pan().onStart(() => {
+    const gesture = Gesture.Pan()
+    .onStart(() => {
         context.value = { y: translateY.value }; 
     }).onUpdate((event) => {
         translateY.value = event.translationY + context.value.y;
-        translateY.value = Math.max(translateY.value, MAX_TRANSLATE_Y );
+        translateY.value = Math.max(translateY.value, MAX_TRANSLATE_Y  );
 
         console.log(translateY.value)
-    });
+    }).onEnd(() => {
+        if (translateY.value > -SCREEN_HEIGHT / 3) {
+            translateY.value = withSpring(0, {damping: 50 })
+        } 
+        else if (translateY.value < -SCREEN_HEIGHT / 1.5) {
+            translateY.value = withSpring(MAX_TRANSLATE_Y, {damping: 50 })
+        
+        }
+    
+    })
 
     const rBottomSheetStyle = useAnimatedStyle(() => {
+        //instead of removing the border radius, make it have a slight spring when you go further than max_translate_y
+        const borderRadius = interpolate(
+            translateY.value,
+            [MAX_TRANSLATE_Y + 50, MAX_TRANSLATE_Y],
+            [25, 5],
+            Extrapolate.CLAMP
+        );
+
          return {
+            borderRadius,
             transform: [{ translateY: translateY.value }],
-            
          } 
     });
  
     useEffect(() => {
-        translateY.value = withSpring(-SCREEN_HEIGHT /3, {damping: 50, duration: 1000 })
+        translateY.value = withSpring(-SCREEN_HEIGHT /3, {damping: 50 })
     }, [])
 
   return ( 
